ae89a5c09af76cff4bf38d7185a34e64a8b01f30
Rubidio24 <31071918+Rubidio24@users.noreply.github.com>
2018-03-24 15:06:45 -0300

    Update fsm.h

--- EDA_TP4/EDA_TP4/fsm.h
+++ EDA_TP4/EDA_TP4/fsm.h
@@ -21,10 +21,10 @@ class fsm
 		void run(int tevento, void* userData);
 	private:
 		const cell tabla[3][8] =
-		{//			MOVE									I_AM_READY				RESEND						   TIMEOUT					 TIMEOUT_2						 ACK					  QUIT									   ERROR
-		{ { SHUTING_DOWN , &print_error },{ SHUTING_DOWN , &print_error },{ SHUTING_DOWN,&print_error},{ WAITING_ACK, &print_resend },{ SHUTING_DOWN ,&print_error },{ WAITING_EVENT, NULL } ,{ SHUTING_DOWN ,&print_error } ,{ SHUTING_DOWN ,&print_error }},	//wait ev
-		{ {WAITING_EVENT,&print_ack},{ WAITING_EVENT, &print_ack },{ WAITING_EVENT,&print_ack },{ SHUTING_DOWN ,&print_error },{ SHUTING_DOWN ,&print_error }, { SHUTING_DOWN ,&print_error }, { SHUTING_DOWN ,&print_error }  ,{ SHUTING_DOWN ,&print_error }},	//wait ack
-		{ {ERROR, &print_error},{ ERROR, &print_error },{ ERROR,&print_error },{ ERROR, &print_error },{ ERROR, &print_error }, { ERROR, &print_error }, { ERROR, &print_error }  ,{ ERROR, &print_error }}	//shutdown
+		{//			MOVE									I_AM_READY						RESEND						   TIMEOUT						 TIMEOUT_2						 ACK								QUIT						  ERROR
+		{ { SHUTING_DOWN , &print_error },	{ SHUTING_DOWN , &print_error },	{ SHUTING_DOWN,&print_error},	{ WAITING_ACK, &print_resend },	{ SHUTING_DOWN ,&print_error },	{ WAITING_EVENT, NULL },		{ SHUTING_DOWN ,&print_error },	{ SHUTING_DOWN ,&print_error }},	//wait ev
+		{ {WAITING_EVENT,&print_ack},		{ WAITING_EVENT, &print_ack },		{ WAITING_EVENT,&print_ack },	{ SHUTING_DOWN ,&print_error },	{ SHUTING_DOWN ,&print_error }, { SHUTING_DOWN ,&print_error },	{ SHUTING_DOWN ,&print_error },	{ SHUTING_DOWN ,&print_error }},	//wait ack
+		{ {ERROR, &print_error},			{ ERROR, &print_error },			{ ERROR,&print_error },			{ ERROR, &print_error },		{ ERROR, &print_error },		{ ERROR, &print_error },		{ ERROR, &print_error },		{ ERROR, &print_error }}	//shutdown
 		};
 		int estado;
 };
