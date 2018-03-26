e36d601a2998e692bedec46cde7997bf82b2daa3
glambertucci <glambertucci@itba.edu.ar>
2018-03-25 23:56:05 -0300

    Added define

--- TP4_EDA_server/TP4_EDA_server/fsm.h
+++ TP4_EDA_server/TP4_EDA_server/fsm.h
@@ -5,6 +5,8 @@
 typedef enum { INIT,WAITING_RD, WAITING_EVENT, WAITING_ACK, SHUTING_DOWN };//testado
 
 typedef void(*pfun)(void* userData);
+#define EVENTS_N 7
+#define STATES_N 5
 
 void print_error(void* userData);
 void print_quit(void* userData);
@@ -28,7 +30,7 @@ class fsm
 {
 
 private:
-	const cell tabla[5][7] =
+	const cell tabla[STATES_N][EVENTS_N] =
 	{//			MOVE				        	I_AM_READY						TIMEOUT						 TIMEOUT_2						 ACK							  QUIT							   ERROR
 	{ { SHUTING_DOWN, &print_error },{ WAITING_RD , &print_imr }   ,{ INIT, &nothing }            ,{ SHUTING_DOWN, &print_error },{ SHUTING_DOWN, &print_error },{ SHUTING_DOWN, &print_quit } ,{ SHUTING_DOWN ,&print_error }}, //init
 	{ { SHUTING_DOWN, &print_error },{ WAITING_ACK , &print_ack_r },{ WAITING_RD, &print_t1_imr } ,{ SHUTING_DOWN, &print_error },{ SHUTING_DOWN, &print_error },{ SHUTING_DOWN, &print_quit } ,{ SHUTING_DOWN ,&print_error }}, //wait rd
